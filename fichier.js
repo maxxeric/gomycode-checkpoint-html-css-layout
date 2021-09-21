3298b23b14e4bbe346c3bbd1134904e36b994cd6
maxxeric <ericbony05@gmail.com>
2021-09-21 18:29:10 +0000

    first commit

---
+++ fichier.js
@@ -0,0 +1,29 @@
+var gourde;
+gourde = "cafe"
+gourde = "leau"
+console.log(gourde);
+var visitorname=prompt('whats your name')
+confirm("are youok")
+console.log('are you ok');
+var person1={
+    name:"bony",
+    age:900,
+    sexe:"male",
+    isstudent:true
+}
+console.log(person1.name);
+console.log(person1.age);
+console.log(person1.sexe);
+console.log(typeof(name));
+console.log(5+7);
+console.log(0/0);
+var num = 8;
+console.log(num)
+num --
+console.log(num)
+num ++
+console.log(num)
+var name="lucy";
+console.log("yo" + name);
+console.log("yo"+ name);
+console.log( "yo", $(name)!);
